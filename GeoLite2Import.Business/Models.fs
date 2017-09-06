325948de991fc7d9ac9c82958096c9063b7e8768
Aleksander Spro <abspro@gmail.com>
2017-09-06 12:00:10 +0200

    Add city and country location models

--- GeoLite2Import.Business/Models.fs
+++ GeoLite2Import.Business/Models.fs
@@ -6,7 +6,7 @@ type CsvColumnAttribute(columnIndex : int) =
     inherit System.Attribute()
     member this.ColumnIndex = columnIndex
 
-type GeoLite2City() =
+type GeoLite2CityBlock() =
     [<CsvColumnAttribute(0)>]
     member val network = "" with get,set
     [<CsvColumnAttribute(1)>]
@@ -28,7 +28,7 @@ type GeoLite2City() =
     [<CsvColumnAttribute(9)>]
     member val accuracy_radius = "" with get,set
 
-type GeoLite2Country() =
+type GeoLite2CountryBlock() =
     [<CsvColumnAttribute(0)>]
     member val network = "" with get,set
     [<CsvColumnAttribute(1)>]
@@ -58,4 +58,46 @@ type WorldCity() =
     [<CsvColumnAttribute(6)>]
     member val Longitude = "" with get,set
     
-        
+type GeoLite2CountryLocation() =
+    [<CsvColumnAttribute(0)>]
+    member val geoname_id = "" with get,set
+    [<CsvColumnAttribute(1)>]
+    member val locale_code = "" with get,set
+    [<CsvColumnAttribute(2)>]
+    member val continent_code = "" with get,set
+    [<CsvColumnAttribute(3)>]
+    member val continent_name = "" with get,set
+    [<CsvColumnAttribute(4)>]
+    member val country_iso_code = "" with get,set
+    [<CsvColumnAttribute(5)>]
+    member val country_name = "" with get,set
+
+type GeoLite2CityLocation() =
+    [<CsvColumnAttribute(0)>]
+    member val geoname_id = "" with get,set
+    [<CsvColumnAttribute(1)>]
+    member val locale_code = "" with get,set
+    [<CsvColumnAttribute(2)>]
+    member val continent_code = "" with get,set
+    [<CsvColumnAttribute(3)>]
+    member val continent_name = "" with get,set
+    [<CsvColumnAttribute(4)>]
+    member val country_iso_code = "" with get,set
+    [<CsvColumnAttribute(5)>]
+    member val country_name = "" with get,set
+    [<CsvColumnAttribute(6)>]
+    member val subdivision_1_iso_code = "" with get,set            
+    [<CsvColumnAttribute(7)>]
+    member val subdivision_1_name = "" with get,set
+    [<CsvColumnAttribute(8)>]
+    member val subdivision_2_iso_code = "" with get,set
+    [<CsvColumnAttribute(9)>]
+    member val subdivision_2_name = "" with get,set
+    [<CsvColumnAttribute(10)>]
+    member val city_name = "" with get,set
+    [<CsvColumnAttribute(11)>]
+    member val metro_code = "" with get,set
+    [<CsvColumnAttribute(12)>]
+    member val time_zone = "" with get,set
+    
+    
